4aedfd9a712b633272f929852b1eb91d9bc66481
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2023-04-10 19:09:50 +0300

    The required part is done.

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,6 +1,10 @@
 const { User } = require("../../models");
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
+
+const { sendEmail } = require("../../helpers");
+const { BASE_URL } = process.env;
 
 const register = async (req, res) => {
   const { email, password, subscription = "starter" } = req.body;
@@ -11,17 +15,31 @@ const register = async (req, res) => {
   }
 
   const avatarURL = gravatar.url(email);
-  const newUser = new User({ email, subscription, avatarURL });
+  const verificationToken = uuidv4();
+  const newUser = new User({
+    email,
+    subscription,
+    avatarURL,
+    verificationToken,
+  });
 
   newUser.setPassword(password);
 
-  newUser.save();
+  await newUser.save();
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/:${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
       email,
       subscription,
       avatarURL,
+      verificationToken,
     },
   });
 };
